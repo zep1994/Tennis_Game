5485c22c7d3fd8274af41074d3261fb3b17c63da
zep1994 <snaxbags@protonmail.com>
2018-11-29 00:43:06 -0600

    Add Canvas

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+var canvas
+var canvasContext
+
+window.onload = function(){
+  canvas = document.getElementById('gameCanvas')
+  canvasContext = canvas.getContext('2d')
+  drawEverything()
+}
+
+function drawEverything() {
+  canvasContext.fillstyle = 'black'
+  canvasContext.fillRect(0,0,canvas.width,canvas.height)
+  canvasContext.fillstyle = 'white'
+  canvasContext.fillRect(225,210,200,200)
+  canvasContext.fillstyle = 'white'
+  canvasContext.fillRect(canvas.width/2,200,50,25)
+}
