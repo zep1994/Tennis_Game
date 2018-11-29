12876a19f1c98453c1a21f839b7b867c006360b0
zep1994 <snaxbags@protonmail.com>
2018-11-29 01:32:26 -0600

    Refactor ball and speed

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-var canvas
-var canvasContext
+var canvas;
+var canvasContext;
+var ballX = 50;
+var ballSpeedX = 5;
 
-window.onload = function(){
-  canvas = document.getElementById('gameCanvas')
-  canvasContext = canvas.getContext('2d')
-  drawEverything()
+window.onload = function() {
+	canvas = document.getElementById('gameCanvas');
+	canvasContext = canvas.getContext('2d');
+
+  var framesPerSecond = 30
+	setInterval(function(){
+    moveEverything()
+    drawEverything()
+  }, 1000/framesPerSecond);
+}
+
+function moveEverything(){
+  ballX = ballX + ballSpeedX;
 }
 
 function drawEverything() {
-  canvasContext.fillstyle = 'black'
-  canvasContext.fillRect(0,0,canvas.width,canvas.height)
-  canvasContext.fillstyle = 'white'
-  canvasContext.fillRect(225,210,200,200)
-  canvasContext.fillstyle = 'white'
-  canvasContext.fillRect(canvas.width/2,200,50,25)
+	console.log(ballX);
+	canvasContext.fillStyle = 'black';
+	canvasContext.fillRect(0,0,canvas.width,canvas.height);
+	canvasContext.fillStyle = 'white';
+	canvasContext.fillRect(0,210,10,100);
+	canvasContext.fillStyle = 'red';
+	canvasContext.fillRect(ballX,100,10,10);
 }
